dbd3934432503fa09fa02db691543913575e9177
roi-777 <61244812+roi-777@users.noreply.github.com>
2020-02-19 11:39:59 -0600

    1

---
+++ page4.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.11.6, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.6, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/19967-image-1-122x122.png" type="image/x-icon">
+  <meta name="description" content="Установка и замена джакузи">
+  
+  <title>Установка и замена джакузи</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rLLd7cRgbs" once="menu" id="menu1-x">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.php">
+                         <img src="assets/images/19967-image-1-122x122.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="index.php">
+                        ГидраСантехСтрой
+                    </a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/n">free simple web templates</a></section><section class="services2 cid-rLLd7dt6Un" id="services2-y">
+    <!---->
+    
+    <!---->
+    <!--Overlay-->
+    
+    <!--Container-->
+    <div class="container">
+        <div class="col-md-12">
+            <div class="media-container-row">
+                <div class="mbr-figure" style="width: 50%;">
+                    <img src="assets/images/usluga-djakuzi300-300x300.jpg" alt="Mobirise" title="">
+                </div>
+                <div class="align-left aside-content">
+                    <h2 class="mbr-title pt-2 mbr-fonts-style display-2">Установка и замена джакузи</h2>
+                    <div class="mbr-section-text">
+                        <p class="mbr-text text1 pt-2 mbr-light mbr-fonts-style display-7">Выезд мастера в день обращения
+<br>Бесплатный осмотр для сметного расчета в пределах МКАД
+<br>Гарантия 1 год на все работы</p>
+                        
+                    </div>
+                    <!--Btn-->
+                    <div class="mbr-section-btn pt-3 align-left"><a href="page4.php#form1-14" class="btn btn-warning-outline display-4">
+                            Заказать работы</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="services5 cid-rLLd7eebhF" id="services5-z">
+    <!---->
+    
+    <!---->
+    <!--Overlay-->
+    
+    <!--Container-->
+    <div class="container">
+        <div class="row">
+            <!--Titles-->
+            <div class="title pb-5 col-12">
+                <h2 class="align-left mbr-fonts-style m-0 display-2">
+                    Цены
+                </h2>
+                
+            </div>
+            <!--Card-1-->
+            <div class="card px-3 col-12">
+                <div class="card-wrapper media-container-row media-container-row">
+                    <div class="card-box">
+                        <div class="top-line pb-3">
+                            <h4 class="card-title mbr-fonts-style display-7">Установка джакузи</h4>
+                            <p class="mbr-text cost mbr-fonts-style m-0 display-7">
+                                		от 1490 р.</p>
+                        </div>
+                        <div class="bottom-line">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Card-2-->
+            <div class="card px-3 col-12">
+                <div class="card-wrapper media-container-row media-container-row">
+                    <div class="card-box">
+                        <div class="top-line pb-3">
+                            <h4 class="card-title mbr-fonts-style display-7">Демонтаж джакузи</h4>
+                            <p class="mbr-text cost mbr-fonts-style m-0 display-7">
+                                			от 490 р.</p>
+                        </div>
+                        <div class="bottom-line">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Card-3-->
+            
+            <!--Card-4-->
+            
+            <!--Card-5-->
+            
+            <!--Card-6-->
+            
+            <!--Card-7-->
+            
+            <!--Card-8-->
+            
+            <!--Card-9-->
+            
+            <!--Card-10-->
+            
+            <!--Card-11-->
+            
+            <!--Card-12-->
+            
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section info2 cid-rLLd7fuS0R" id="info2-10">
+
+    
+
+    
+
+    <div class="container">
+        <div class="row main justify-content-center">
+            <div class="media-container-column col-12 col-lg-3 col-md-4">
+                
+            </div>
+            <div class="media-container-column title col-12 col-lg-7 col-md-6">
+                <h2 class="align-right mbr-bold mbr-white pb-3 mbr-fonts-style display-2">
+                    Акция
+                </h2>
+                <h3 class="mbr-section-subtitle align-right mbr-light mbr-white mbr-fonts-style display-5">При замене труб от 15000 рублей, установка (замена) 2-х счетчиков воды бесплатно.</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features1 cid-rLLd7g5xL8" id="features1-11">
+    
+    
+
+    
+    <div class="container">
+        <div class="media-container-row">
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-clock"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Срочный выезд
+</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Выезд мастера в день обращения
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-like"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Гарантия и качество
+</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Мы даем 12 месяцев гарантии на все наши работы
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-briefcase"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">
+                        Профессионализм
+                    </h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Дипломированные специалисты. Официальная организация – аттестат аккредитации №RA.RU.312720.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-search"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Бесплатный осмотр
+</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       Выезд мастера для сметного расчета в пределах МКАД бесплатно
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="features1 cid-rLLd7h4cQt" id="features1-12">
+    
+    
+
+    
+    <div class="container">
+        <div class="media-container-row">
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-smile-face"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Адекватные цены
+</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        У нас самые низкие цены. Работаем по безналичному расчету с физическими и с юридическими лицами
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-sale"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Система скидок</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Инвалидам, инвалидам ВОВ, многодетным семьям. Предоставляем скидки на коллективные заявки
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-paper-plane"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Москва и Московская область</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Мы работаем по всей Москве и Московской области
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-file"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-5">Комплект документов</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       Мы выписываем полный пакет документов для Москвы и Московской области
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="mbr-section article content9 cid-rLLd7hSFqO" id="content9-13">
+    
+     
+
+    <div class="container">
+        <div class="inner-container" style="width: 100%;">
+            <hr class="line" style="width: 25%;">
+            <div class="section-text align-center mbr-fonts-style display-7">Мы на связи!
+<div>С радостью ответим на все ваши вопросы по телефону:
+</div><div>
+</div><div>+7 (499) 678-42-22
+</div><div><br></div><div>Время работы: с 08.00 до 20.00 (Пн-сб), с 11.00 до 17.00 (Вс)
+</div><div>
+</div><div>Наш адрес: 127427, г. Москва, ул.Ботаническая, дом 19, корпус 1
+</div><div>
+</div><div>Мы гарантируем конфинденциальность, честные цены и профессиональный сервис</div></div>
+            <hr class="line" style="width: 25%;">
+        </div>
+        </div>
+</section>
+
+<section class="mbr-section form1 cid-rLLd7ilwsy" id="form1-14">
+
+    
+
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">Оставьте заявку в этой форме и наш звонок не заставит долго ждать!</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                <!---Formbuilder Form--->
+                <form action="https://mobirise.com/" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="ecVpKHBd0zFua8GOcdW3mPdu3/vxRCNCteqYiLsj0V5mj4MR0nRIe/j5in2TVf3G26kAcPXU9raz/yD/d2hW6SNrxWnt7pC+Or3T2J14p8o9ECLbq3wtcT0y1n+xXtwD">
+                    <div class="row">
+                        <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Спасибо за заполнение анкеты!</div>
+                        <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                        </div>
+                    </div>
+                    <div class="dragArea row">
+                        <div class="col-md-4  form-group" data-for="name">
+                            <label for="name-form1-14" class="form-control-label mbr-fonts-style display-7">Имя</label>
+                            <input type="text" name="name" data-form-field="Name" required="required" class="form-control display-7" id="name-form1-14">
+                        </div>
+                        <div class="col-md-4  form-group" data-for="email">
+                            <label for="email-form1-14" class="form-control-label mbr-fonts-style display-7">Email</label>
+                            <input type="email" name="email" data-form-field="Email" required="required" class="form-control display-7" id="email-form1-14">
+                        </div>
+                        <div data-for="phone" class="col-md-4  form-group">
+                            <label for="phone-form1-14" class="form-control-label mbr-fonts-style display-7">Телефон</label>
+                            <input type="tel" name="phone" data-form-field="Phone" class="form-control display-7" id="phone-form1-14">
+                        </div>
+                        <div data-for="message" class="col-md-12 form-group">
+                            <label for="message-form1-14" class="form-control-label mbr-fonts-style display-7">Сообщение</label>
+                            <textarea name="message" data-form-field="Message" class="form-control display-7" id="message-form1-14"></textarea>
+                        </div>
+                        <div class="col-md-12 input-group-btn align-center"><button type="submit" class="btn btn-primary btn-form display-4">Заказать работы</button></div>
+                    </div>
+                </form><!---Formbuilder Form--->
+            </div>
+        </div>
+    </div>
+</section>
+
+<section once="footers" class="cid-rLLxEdAL3w" id="footer6-2c">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    © Copyright 2019 ГидраСантехСтрой - Все права защищены<br><a href="Privacy policy.php" class="text-success">Политика Конфиденциальности</a><br><a href="Disclaimer.php" class="text-success"> &nbsp; Отказ от ответственности</a><br>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+</body>
+</html>
